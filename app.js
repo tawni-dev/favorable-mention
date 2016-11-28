d6e32bd2dd5b0a170518d67124c6cd6ccdd70038
equinoxjenkins <equinoxjenkins@gmail.com>
2016-11-28 14:48:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+angular.module('favorableMention', ['ui.router']);
+
+favorableMention.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('home', {
+            url: '/',
+            templateUrl: 'views/home/homeTempl.html'
+        })
+        .state('music', {
+            url: '/music',
+            templateUrl: 'views/music/musicTempl.html'
+        });
+});
---
+++ js/homeCtrl.js
@@ -0,0 +1,5 @@
+angular.module('favorableMention').controller('homeCtrl', function($scope) {
+
+
+
+});
